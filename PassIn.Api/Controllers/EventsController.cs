2cfeca18645db60d07cdc102fe8a7dffb75187c3
erickcred <erickcredjes@gmail.com>
2024-04-03 20:25:33 -0300

    Iniciando projeto

---
+++ PassIn.Api/Controllers/EventsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PassIn.Application.UseCases.Events.Rgister;
+using PassIn.Communication.Requests;
+using System.Text.Json;
+
+namespace PassIn.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EventsController : ControllerBase
+{
+  private readonly ILogger _logger;
+  private readonly RegisterEventUseCase _registerEventUseCase;
+
+  public EventsController(
+    RegisterEventUseCase registerEventUseCase,
+    ILogger logger)
+  {
+    _registerEventUseCase = registerEventUseCase;
+    _logger = logger;
+  }
+
+  [HttpPost]
+  public IActionResult Register([FromBody] RequestEventJson request)
+  {
+    try
+    {
+      var useCase = _registerEventUseCase;
+      useCase.Execute(request);
+    
+      return Created();
+    } catch (ArgumentException ex)
+    {
+      _logger.LogError(JsonSerializer.Serialize(ex));
+      return BadRequest(ex.Message);
+    } catch (Exception ex)
+    {
+      _logger.LogError(JsonSerializer.Serialize(ex));
+      return null;
+    }
+  }
+}
---
+++ PassIn.Application/UseCases/Events/Rgister/RegisterEventUseCase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using PassIn.Communication.Requests;
+
+namespace PassIn.Application.UseCases.Events.Rgister;
+
+public class RegisterEventUseCase
+{
+  private readonly ILogger _logger;
+
+  public RegisterEventUseCase(ILogger logger)
+  {
+    _logger = logger;
+  }
+
+  public void Execute(RequestEventJson request)
+  {
+    Validate(request);
+  }
+
+  private void Validate(RequestEventJson request)
+  {
+    if (request.MaximumAttendees < 1)
+      throw new ArgumentException("O número maximo de participantes é invalido!");
+
+    if (string.IsNullOrWhiteSpace(request.Title))
+      throw new ArgumentException("O Título é invalido!");
+
+    if (string.IsNullOrWhiteSpace(request.Details))
+      throw new ArgumentException("O Detalhe é invalido!");
+  }
+}
+
+
