7a99ec77dcb2c049a69f66e7ba23e9317a0c453e
Erick Rick <erickcredjes@gmail.com>
2024-04-06 11:38:55 -0300

    Implementado Busca Evento Por Id

---
+++ PassIn.Application/UseCases/Events/GetById/GetEventByIdUseCase.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PassIn.Communication.Responses;
+using PassIn.Exceptions;
+using PassIn.Infrastructure;
+
+namespace PassIn.Application.UseCases.Events.Get;
+
+public class GetEventByIdUseCase
+{
+
+  private readonly PassInContext _context;
+
+  public GetEventByIdUseCase(PassInContext context)
+  {
+    _context = context;
+  }
+
+  public ResponseEventJson Execute(int id)
+  {
+    return ReturneEvet(id);
+  }
+
+  private ResponseEventJson ReturneEvet(int id)
+  {
+    var dataBase = _context.Events.AsNoTracking().FirstOrDefault(e => e.Id == id);
+    if (dataBase is null)
+      throw new PassInException("Não foi possível localizar esse evento!");
+
+    var responseEvent = new ResponseEventJson
+    {
+      Id = dataBase.Id,
+      Title = dataBase.Title,
+      Details = dataBase.Details,
+      AttendeesAmount = 0,
+      MaximumAttendees = dataBase.MaximumAttendees
+    };
+
+    return responseEvent;
+  }
+}
